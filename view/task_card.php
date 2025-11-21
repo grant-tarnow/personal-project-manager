507465f40d4f13080c7474d209302eb85bbff0f2
gt <>
2025-11-21 09:52:36 -0500

    switch task card to flexbox

--- view/task_card.php
+++ view/task_card.php
@@ -5,65 +5,65 @@ $color = statusColor($task['status']);
 ?>
 
 <div class="task-card" id="<?= "task{$task['task_id']}" ?>">
-    <div class="task-card-header">
-        <h3><?= $task['description'] ?></h3>
-    </div>
-    <div class="task-card-left">
-        <h3>
-        <?php if ($task['next']): ?>
-            <span style="color: firebrick;">NEXT</span>
-        <?php else: ?>
-            <form class="just-btn" action="/?action=nextify-from-project" method="POST" >
+    <h3><?= $task['description'] ?></h3>
+    <div class="task-card-body">
+        <div class="task-card-main">
+            <h3>
+            <?php if ($task['next']): ?>
+                <span style="color: firebrick;">NEXT</span>
+            <?php else: ?>
+                <form class="just-btn" action="/?action=nextify-from-project" method="POST" >
+                    <input type="hidden" name="tid" value="<?= $task['task_id'] ?>" />
+                    <input type="hidden" name="pid" value="<?= $task['project_id'] ?>" />
+                    <button type="submit" >nextify</button>
+                </form>
+            <?php endif; ?>
+            |
+            <span style="color: <?= $color ?>"><?= $task['status'] ?></span>
+            |
+            <?php if (checkQueued("task", $task['task_id'])): ?>
+            QUEUED
+            <?php else: ?>
+                <form class="just-btn" action="/?action=queue-task-from-project" method="POST" >
+                    <input type="hidden" name="tid" value="<?= $task['task_id'] ?>" />
+                    <input type="hidden" name="pid" value="<?= $task['project_id'] ?>" />
+                    <button type="submit" >queue</button>
+                </form>
+            <?php endif; ?>
+            </h3>
+            <table>
+                <tr>
+                    <td>due:</td>
+                    <td><?= $task['due'] ?></td>
+                </tr>
+                <tr>
+                    <td>notes:</td>
+                    <td><?= count($task_notes) ?></td>
+                </tr>
+                <tr>
+                    <td>last update:</td>
+                    <td><?= $task_updates[0]['created'] ?? $task['updated'] ?></td>
+                </tr>
+                <tr>
+                    <td>created:</td>
+                    <td><?= $task['created'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <div class="task-card-controls">
+            <h4>MOVE</h4>
+            <form class="just-btn" action="/?action=move-task-up" method="POST">
                 <input type="hidden" name="tid" value="<?= $task['task_id'] ?>" />
                 <input type="hidden" name="pid" value="<?= $task['project_id'] ?>" />
-                <button type="submit" >nextify</button>
+                <button type="submit" >up</button>
             </form>
-        <?php endif; ?>
-        |
-        <span style="color: <?= $color ?>"><?= $task['status'] ?></span>
-        |
-        <?php if (checkQueued("task", $task['task_id'])): ?>
-        QUEUED
-        <?php else: ?>
-            <form class="just-btn" action="/?action=queue-task-from-project" method="POST" >
+            <br>
+            <form class="just-btn" action="/?action=move-task-down" method="POST">
                 <input type="hidden" name="tid" value="<?= $task['task_id'] ?>" />
                 <input type="hidden" name="pid" value="<?= $task['project_id'] ?>" />
-                <button type="submit" >queue</button>
+            <button type="submit" >dn</button>
             </form>
-        <?php endif; ?>
-        </h3>
-        <table>
-            <tr>
-                <td>due:</td>
-                <td><?= $task['due'] ?></td>
-            </tr>
-            <tr>
-                <td>notes:</td>
-                <td><?= count($task_notes) ?></td>
-            </tr>
-            <tr>
-                <td>last update:</td>
-                <td><?= $task_updates[0]['created'] ?? $task['updated'] ?></td>
-            </tr>
-            <tr>
-                <td>created:</td>
-                <td><?= $task['created'] ?></td>
-            </tr>
-        </table>
-    </div>
-    <div class="task-card-right">
-        <h4>MOVE</h4>
-        <form class="just-btn" action="/?action=move-task-up" method="POST">
-            <input type="hidden" name="tid" value="<?= $task['task_id'] ?>" />
-            <input type="hidden" name="pid" value="<?= $task['project_id'] ?>" />
-            <button type="submit" >up</button>
-        </form>
-        <br>
-        <form class="just-btn" action="/?action=move-task-down" method="POST">
-            <input type="hidden" name="tid" value="<?= $task['task_id'] ?>" />
-            <input type="hidden" name="pid" value="<?= $task['project_id'] ?>" />
-        <button type="submit" >dn</button>
-        </form>
+        </div>
     </div>
 </div>
 <script>
